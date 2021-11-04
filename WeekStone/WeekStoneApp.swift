96ffd53027b58eaae5cd101a806860de6e4f4382
Grzegorz <workbaranowski@gmail.com>
2021-11-04 18:40:36 +0100

    Initial Commit

---
+++ WeekStone/WeekStoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekStoneApp.swift
+//  WeekStone
+//
+//  Created by Grzegorz Baranowski on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeekStoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
